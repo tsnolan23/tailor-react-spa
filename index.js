4fbaade5eb5d751d48f36003a8ce76f3747da233
Dawid Winiarczyk <dawid.winiarczyk@pracuj.pl>
2018-09-11 23:10:03 +0200

    introducing jagger

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 'use strict'
 
 const Tailor = require('node-tailor')
+const { initTracer, PrometheusMetricsFactory, ConstSampler } = require('jaeger-client');
+const promClient = require('prom-client');
+const bunyan = require('bunyan');
+
+const config = {
+	serviceName: 'my:awesome:service'
+};
+const namespace = config.serviceName;
+const metrics = new PrometheusMetricsFactory(promClient, namespace);
+
+const logger = bunyan.createLogger({
+	name: 'hello-world'
+});
 const tailor = new Tailor({
-  templatesPath: __dirname + '/templates'
+  templatesPath: __dirname + '/templates',
+	/*
+	docker run -d -p5775:5775/udp -p6831:6831/udp -p6832:6832/udp -p16686:16686 jaegertracing/all-in-one:latest
+	 https://github.com/jaegertracing/jaeger-client-node/issues/121
+	 */
+	tracer: initTracer(
+		config,
+		{
+			host: '127.0.0.1',
+			port: 6832,
+			metrics,
+			logger,
+			sampler: new ConstSampler(true)
+		})
 })
 
 module.exports = (req, res) => {
