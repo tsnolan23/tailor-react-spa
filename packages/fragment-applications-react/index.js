3675910cc873ec6f5eacc8aa4309dfbed9b2b863
Dawid Winiarczyk <dawid.winiarczyk@pracuj.pl>
2018-09-14 22:38:46 +0200

    declare streams out of executing fragments

--- packages/fragment-applications-react/index.js
+++ packages/fragment-applications-react/index.js
@@ -5,8 +5,12 @@ require('@babel/register')({
 });
 
 const url = require('url')
-const fs = require('fs')
+const { createReadStream } = require('fs')
 const renderStream = require('./render-stream.js');
+
+
+const bundleStream = createReadStream('./dist/bundle.js');
+const markupStream = renderStream();
 
 module.exports = (req, res) => {
 	const pathname = url.parse(req.url).pathname
@@ -14,7 +18,7 @@ module.exports = (req, res) => {
 	switch(pathname) {
 		case '/dist/bundle.js':
 			res.writeHead(200, jsHeader)
-      fs.createReadStream('./dist/bundle.js').pipe(res)
+      bundleStream.pipe(res)
       break;
 		default:
 			res.writeHead(200, {
@@ -22,7 +26,7 @@ module.exports = (req, res) => {
 				'Link': '<http://localhost:1234/dist/bundle.js>; rel="fragment-script"'
 			})
 
-			renderStream().then(({ stream, state }) => {
+			markupStream.then(({ stream, state }) => {
 				res.write(`
      			<script>window.CONTACTS_STATE = ${JSON.stringify(state).replace(/</g, '\\\u003c')}</script>
       `);
--- packages/fragment-recommendations-vue2/index.js
+++ packages/fragment-recommendations-vue2/index.js
@@ -1,8 +1,11 @@
 const url = require('url')
-const fs = require('fs')
+const { createReadStream } = require('fs')
 
 const renderStream = require('./render-stream.js');
 
+
+const bundleStream = createReadStream('./dist/bundle.js');
+const markupStream = renderStream();
 
 module.exports = (req, res) => {
 	const pathname = url.parse(req.url).pathname
@@ -10,7 +13,7 @@ module.exports = (req, res) => {
 	switch(pathname) {
 		case '/dist/bundle.js':
 			res.writeHead(200, jsHeader)
-			fs.createReadStream('./dist/bundle.js').pipe(res)
+			bundleStream.pipe(res)
 			break;
 		default:
 			res.writeHead(200, {
@@ -18,6 +21,6 @@ module.exports = (req, res) => {
 				'Link': '<http://localhost:5655/dist/bundle.js>; rel="fragment-script"'
 			})
 
-			renderStream().pipe(res);
+			markupStream.pipe(res);
 	}
 };
