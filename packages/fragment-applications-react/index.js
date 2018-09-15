a733bab0b85a41b1b0694ccbcc5b97862d4c3ee1
Dawid Winiarczyk <dawid.winiarczyk@pracuj.pl>
2018-09-15 11:49:16 +0200

    bump

--- packages/fragment-applications-react/index.js
+++ packages/fragment-applications-react/index.js
@@ -9,15 +9,13 @@ const { createReadStream } = require('fs')
 const renderStream = require('./render-stream.js');
 
 
-const bundleStream = createReadStream('./dist/bundle.js');
-
 module.exports = (req, res) => {
 	const pathname = url.parse(req.url).pathname
 	const jsHeader = { 'Content-Type': 'application/javascript' }
 	switch(pathname) {
 		case '/dist/bundle.js':
 			res.writeHead(200, jsHeader)
-      bundleStream.pipe(res)
+			createReadStream('./dist/bundle.js').pipe(res)
       break;
 		default:
 			res.writeHead(200, {
