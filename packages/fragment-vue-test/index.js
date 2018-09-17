47c47eee1ccee26f0fc756290cf4114d149bd396
wjureczka <wjureczka@gmail.com>
2018-09-17 22:29:30 +0200

    Update: Init second test

---
+++ packages/fragment-vue-test/index.js
@@ -0,0 +1,22 @@
+const express = require('express')();
+const { createReadStream } = require('fs')
+
+const renderStream = require('./render-stream.js')
+const bundleStream = createReadStream('./dist/bundle.js');
+
+
+express.all('/', ((req, res) => {
+    res.set({
+        'Content-Type': 'text/html',
+        'Link': '<http://localhost:5655/dist/bundle.js>; rel="fragment-script"' // https://www.w3.org/wiki/LinkHeader
+    })
+    renderStream().pipe(res);
+}));
+
+express.all('/dist/bundle.js', (req, res) => {
+	res.set({ 'Content-Type': 'application/javascript' });
+	bundleStream.pipe(res);
+});
+
+
+express.listen(5655);
