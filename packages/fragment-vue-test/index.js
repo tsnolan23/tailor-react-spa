5aaaefdf3e92e552e72331ae89f79e92050e9013
PRACUJ\dawid.winiarczyk <dawid.winiarczyk@pracuj.pl>
2018-09-18 15:30:13 +0200

    rename id

--- packages/fragment-vue-test/index.js
+++ packages/fragment-vue-test/index.js
@@ -1,22 +1,13 @@
 const express = require('express')();
-const { createReadStream } = require('fs')
 
 const renderStream = require('./render-stream.js')
-const bundleStream = createReadStream('./dist/bundle.js');
 
 
 express.all('/', ((req, res) => {
     res.set({
         'Content-Type': 'text/html',
-        'Link': '<http://localhost:5666/dist/bundle.js>; rel="fragment-script"' // https://www.w3.org/wiki/LinkHeader
     })
     renderStream().pipe(res);
 }));
-
-express.all('/dist/bundle.js', (req, res) => {
-	res.set({ 'Content-Type': 'application/javascript' });
-	bundleStream.pipe(res);
-});
-
 
 express.listen(5666);
