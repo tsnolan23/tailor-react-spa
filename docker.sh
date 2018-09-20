08e2ce373461a7e8c9f2339950804a990c7bab6f
Dawid Winiarczyk <dawid.winiarczyk@pracuj.pl>
2018-09-20 21:16:20 +0200

    bump

--- docker.sh
+++ docker.sh
@@ -5,10 +5,15 @@ sleep 5
 
 docker-compose up -d jaeger
 echo "running jaeger at http://localhost:16686"
-sleep 5
 
 docker-compose up -d kibana
 echo "running kibana at http://localhost:5601"
+
+docker-compose up -d grafana
+echo "running grafana at http://localhost:3000"
+
+docker-compose up -d prometheus
+echo "running prometheus at http://localhost:9090"
 sleep 5
 
 docker-compose up -d
--- index.js
+++ index.js
@@ -1,39 +1,26 @@
 const Tailor = require('node-tailor')
-const { initTracer, PrometheusMetricsFactory, ProbabilisticSampler } = require('jaeger-client');
-const promClient = require('prom-client');
-const bunyan = require('bunyan');
+const { initTracer, PrometheusMetricsFactory, ProbabilisticSampler } = require('jaeger-client')
+const promClient = require('prom-client')
+const bunyan = require('bunyan')
 
 const config = {
 	serviceName: 'my:awesome:service',
 	reporter: {
-		agentHost: "jaeger"
+		agentHost: 'jaeger'
 	}
-};
-const namespace = config.serviceName;
-const metrics = new PrometheusMetricsFactory(promClient, namespace);
+}
+const namespace = config.serviceName
+const metrics = new PrometheusMetricsFactory(promClient, namespace)
 
 const logger = bunyan.createLogger({
 	name: namespace
-});
+})
 const tailor = new Tailor({
   templatesPath: __dirname + '/templates',
-	/*
-
-konsul
-
-grafana
-docker run -d --name=grafana -p 3000:3000 grafana/grafana
-
-zipkin (nieuzywany)
-docker run -d -p 9411:9411 openzipkin/zipkin
-
-prometeus
-https://prometheus.io/docs/prometheus/latest/getting_started/
-	 */
 	tracer: initTracer(
 		config,
 		{
-			host: "jaeger",
+			host: 'jaeger',
 			sampler: new ProbabilisticSampler(1),
 			metrics,
 			logger
@@ -44,11 +31,11 @@ module.exports = (req, res) => {
 	if (req.url === '/favicon.ico') {
 		res.writeHead(200, { 'Content-Type': 'image/x-icon' })
 		res.end('')
-	  return;
+	  return
 	}
 
 	req.headers['x-request-uri'] = req.url
 	req.url = '/index'
 
 	tailor.requestHandler(req, res)
-};
+}
