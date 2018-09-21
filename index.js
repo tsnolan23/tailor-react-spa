d38f07aee2ca6367145cdf4723047e0fd67afaf1
PRACUJ\dawid.winiarczyk <dawid.winiarczyk@pracuj.pl>
2018-09-21 17:04:46 +0200

    bump

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const Tailor = require('node-tailor')
 const { initTracer, PrometheusMetricsFactory, ProbabilisticSampler } = require('jaeger-client')
 const promClient = require('prom-client')
 const bunyan = require('bunyan')
+const consul = require('consul')({
+  host: 'consul'
+});
 
 const config = {
 	serviceName: 'my:awesome:service',
@@ -28,6 +31,17 @@ const tailor = new Tailor({
 })
 
 module.exports = (req, res) => {
+  consul.agent.service.list(function(err, result) {
+  	Object.values(result)
+			.filter(({ Service }) => Service === 'test2')
+			.forEach(({ Address }) => {
+				console.log(Address);
+
+				// consul.agent.join(Address, ((er, res) => {
+				// 	console.log(er, res);
+				// }));
+			});
+  });
 	if (req.url === '/favicon.ico') {
 		res.writeHead(200, { 'Content-Type': 'image/x-icon' })
 		res.end('')
--- packages/fragment-vue-http/index.js
+++ packages/fragment-vue-http/index.js
@@ -1,18 +1,10 @@
 const renderStream = require('./render-stream.js')
-var consul = require("consul")({
+var consul = require('consul')({
   host: 'consul'
 });
 const { machineIdSync } = require('node-machine-id');
 var ip = require("ip");
-consul.agent.service.register({
-  id: machineIdSync(),
-  name: 'test2',
-  address: ip.address(),
-  port: 80
-}, (err, t) => {
-  console.log(err);
-  console.log(t);
-})
+
 
 module.exports = (req, res) => {
   consul.agent.service.register({
@@ -24,19 +16,7 @@ module.exports = (req, res) => {
     console.log(err);
     console.log(t);
   })
-  // const payload = {
-  //   Node: machineIdSync(),
-  //   Address: ip.address(),
-  //   ServiceName: 'Vue-http',
-  //   ServiceAddress: 'fragment-vue-http',
-  //   ServicePort: 80
-  // };
-  //
-  // axios.put('http://consul:8500/v1/catalog/register', {...payload})
-  //   .then((response) => {
-  //     console.log(response.data);
-  //   })
-  //   .catch(console.log)
+
 
   res.writeHead(200, {
     'Content-Type': 'text/html'
