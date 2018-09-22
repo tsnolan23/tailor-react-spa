090fb4dca778e8fd3b5b6e9626b772b66f862159
Dawid Winiarczyk <dawid.winiarczyk@pracuj.pl>
2018-09-22 15:43:47 +0200

    bump

--- environment.js
+++ environment.js
@@ -1,10 +1,13 @@
 const setConsul = (consulAddress) => ({ consulAddress })
+const setJaeger = (jaegerAddress) => ({ jaegerAddress })
 
 const { env } = process
 const {
+	JAEGER_HOST,
 	CONSUL_HOST
 } = env
 
 module.exports = ({
+	...setJaeger(JAEGER_HOST),
 	...setConsul(CONSUL_HOST)
 })
