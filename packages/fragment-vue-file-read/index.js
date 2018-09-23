bad5e9a8ca4563e8e78e2550e0f8ac1dfc2d2a35
Dawid Winiarczyk <dawid.winiarczyk@pracuj.pl>
2018-09-23 20:48:12 +0200

    bump

--- packages/fragment-vue-file-read/index.js
+++ packages/fragment-vue-file-read/index.js
@@ -1,26 +1,45 @@
 const consul = require('consul')
+const { initTracer, PrometheusMetricsFactory, ProbabilisticSampler } = require('jaeger-client')
+const promClient = require('prom-client')
+const bunyan = require('bunyan')
 
 const renderStream = require('./render-stream.js')
 const { consulAddress, address, hostname, port } = require('./environment.js')
 
+const tracingAddress = 'jaeger'
+const serviceName = 'aaa:test'
 
-const { agent } = consul({
-	host: consulAddress,
-	promisify: true
-})
-
-agent.service.register({
-	name: hostname,
-	address,
-	port
-})
-	.catch((e) => {
-		'logowanie do spana'
+const d = initTracer(
+	{
+		serviceName,
+		reporter: {
+			agentHost: tracingAddress
+		}
+	},
+	{
+		host: tracingAddress,
+		sampler: new ProbabilisticSampler(1),
+		metrics: new PrometheusMetricsFactory(promClient, serviceName),
+		logger: bunyan.createLogger({
+			name: serviceName
+		})
 	})
 
-module.exports = (request, response) => {
+
+
+module.exports = (request, response, a, b) => {
+	const { globalTracer, Tags, FORMAT_HTTP_HEADERS } = require('opentracing');
+	const tracer = globalTracer()
+console.log(request.headers)
+	const span = d.startSpan('teststata_asdaa');
+	span.addTags({
+		[Tags.HTTP_URL]: 'teateatae',
+		[Tags.SPAN_KIND]: Tags.SPAN_KIND_RPC_SERVER
+	});
+
 	response.writeHead(200, {
     'Content-Type': 'text/html'
   })
   renderStream().pipe(response)
+	span.finish();
 }
