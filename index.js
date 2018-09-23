5eec008dd4df369ed03407c67876bfe03c200f70
Dawid Winiarczyk <dawid.winiarczyk@pracuj.pl>
2018-09-23 13:49:29 +0200

    tailor to separated file

--- index.js
+++ index.js
@@ -1,76 +1,34 @@
-const Tailor = require('node-tailor')
 const { initTracer, PrometheusMetricsFactory, ProbabilisticSampler } = require('jaeger-client')
 const promClient = require('prom-client')
 const bunyan = require('bunyan')
 const consul = require('consul')
 
 const { serviceName, tracingAddress, consulAddress } = require('./environment.js')
+const microservices = require('./microservices.js')
 
 
-const { agent } = consul({
-	host: consulAddress,
-	promisify: true
-})
+const { requestHandler } = microservices(
+	consul({
+		host: consulAddress,
+		promisify: true
+	}),
 
-const config = {
-	serviceName,
-	reporter: {
-		agentHost: tracingAddress
-	}
-}
-const metrics = new PrometheusMetricsFactory(promClient, serviceName)
-const logger = bunyan.createLogger({
-	name: serviceName
-})
-const { requestHandler } = new Tailor({
-	handledTags: ['script'],
-	handleTag(request, tag, options, context) {
-		if (tag.attributes && tag.attributes.type === 'tailor/plugin') {
-			return `<script>
-								(function (d) {
-						var i;
-						require(d);
-						var arr = ['react', 'react-dom', 'react-redux', 'redux', 'prop-types', 'classnames', 'vue', 'vuex', 'axios'];
-						while (i = arr.pop()) (function (dep) {
-							define(dep, d, function (b) {
-								return b[dep];
-							})
-						})(i);
-					}(['${context['service-common'].src}/dist/bundle.js']));
-			</script>`
-		}
-
-		return ''
-	},
-	fetchContext: async () => {
-		const [ services, error ] = await agent.service.list()
-			.then((result) => [ result ])
-			.catch((error) => [, error ])
-
-		error && 'do spana i zabic serwer'
-
-		const urls = Object.values(services)
-			.map(({ Address, Port }) => 'http://' + Address + ':' + Port)
-
-		return Promise.resolve(
-			Object.keys(services)
-				.reduce((prev, curr, index) => ({
-					...prev,
-					[curr]: {
-						src: urls[index]
-					}
-				}), {})
-		)
-	},
-	tracer: initTracer(
-		config,
+	initTracer(
+		{
+			serviceName,
+			reporter: {
+				agentHost: tracingAddress
+			}
+		},
 		{
 			host: tracingAddress,
 			sampler: new ProbabilisticSampler(1),
-			metrics,
-			logger
+			metrics: new PrometheusMetricsFactory(promClient, serviceName),
+			logger: bunyan.createLogger({
+				name: serviceName
+			})
 		})
-})
+)
 
 module.exports = async (request, response) => {
 	if (request.url === '/favicon.ico') {
