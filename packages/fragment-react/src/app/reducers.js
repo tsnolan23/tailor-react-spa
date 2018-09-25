971312bec887481caf8ecfe08d86735ac2983f54
PRACUJ\dawid.winiarczyk <dawid.winiarczyk@pracuj.pl>
2018-09-25 16:15:52 +0200

    bump

--- packages/fragment-react/src/app/reducers.js
+++
@@ -1,7 +0,0 @@
-const initialState = [];
-
-const reducer = (state = initialState, action) => {
-	return state;
-}
-
-export default reducer;
--- packages/fragment-react/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'redux';
-
-import reducers from './reducers.js';
-
-
-export default function create(state) {
-	return createStore(reducers, state);
-};
