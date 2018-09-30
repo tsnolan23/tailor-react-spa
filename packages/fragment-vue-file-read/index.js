20f3c08b37399c928148379e0271ab6d6591f9ce
Dawid Winiarczyk <dawid.winiarczyk@pracuj.pl>
2018-09-30 23:55:19 +0200

    Update index.js

--- packages/fragment-vue-file-read/index.js
+++ packages/fragment-vue-file-read/index.js
@@ -1,5 +1,5 @@
 const consul = require('consul')
-const { readFileSync } = require('fs')
+const { createReadStream } = require('fs')
 const { initTracer, PrometheusMetricsFactory, ProbabilisticSampler } = require('jaeger-client')
 const promClient = require('prom-client')
 const bunyan = require('bunyan')
@@ -42,13 +42,14 @@ agent.service.register({
 	})
 
 module.exports = (request, response) => {
-	  response.writeHead(200, {
-    'Content-Type': 'text/html'
-  })
-	
-	const file = readFileSync('./file-to-read.txt')
-	
-	response.write(file)
+
+	response.writeHead(200, {
+		'Content-Type': 'text/html'
+	})
+
+	const file = createReadStream('file-to-read.txt')
+
+	file.pipe(response);
 
 	const parentSpanContext = tracer.extract(
 		FORMAT_HTTP_HEADERS,
